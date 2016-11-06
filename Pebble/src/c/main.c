fd71c20f40e5b7041955208f1572d6417de4784d
jchen790 <jchen790@users.noreply.github.com>
2016-11-06 11:36:30 -0500

    Update main.c

--- Pebble/src/c/main.c
+++ Pebble/src/c/main.c
@@ -11,6 +11,17 @@ static char* default_message =
 "UP: save place \n\n SELECT: help \n\n DOWN: see saved places";
 
 
+static GBitmap *s_bitmap;
+static BitmapLayer *s_bitmap_layer;
+
+
+static const uint32_t FOX_ICON[] = {
+  RESOURCE_ID_FOX_NONE,
+  RESOURCE_ID_FOX_SAVE,
+  RESOURCE_ID_FOX_HELP,
+  RESOURCE_ID_FOX_LIST
+};
+
 static void send(int key, int value) {
   DictionaryIterator *iter;
   app_message_outbox_begin(&iter);
@@ -27,6 +38,9 @@ static void outbox_sent_handler(DictionaryIterator *iter, void *context) {
   text_layer_set_text_color(s_output_layer, GColorWhite);
   text_layer_set_background_color(s_output_layer, GColorRed);
   window_set_background_color(s_main_window, GColorRed);
+  
+  gbitmap_destroy(s_bitmap);
+  s_bitmap = gbitmap_create_with_resource(FOX_ICON[0]);
 }
 
 static void outbox_failed_handler(DictionaryIterator *iter, AppMessageResult reason, void *context) {
@@ -41,6 +55,11 @@ static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_background_color(s_output_layer, GColorRed);
   window_set_background_color(s_main_window, GColorRed);
   text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
+  
+  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOX_SAVE);
+  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpAssign);
+  bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+  bitmap_layer_set_alignment(s_bitmap_layer, GAlignCenter);
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -50,15 +69,25 @@ static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_background_color(s_output_layer, GColorRed);
   window_set_background_color(s_main_window, GColorRed);
   text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
+  
+  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOX_HELP);
+  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpAssign);
+  bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+  bitmap_layer_set_alignment(s_bitmap_layer, GAlignCenter);
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   send(KEY_BUTTON_DOWN, 0);
-  text_layer_set_text(s_output_layer, "Saved places on phone!");
+  text_layer_set_text(s_output_layer, "See saved places on phone!");
   text_layer_set_text_color(s_output_layer, GColorWhite);
   text_layer_set_background_color(s_output_layer, GColorRed);
   window_set_background_color(s_main_window, GColorRed);
   text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
+  
+  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FOX_LIST);
+  bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpAssign);
+  bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
+  bitmap_layer_set_alignment(s_bitmap_layer, GAlignCenter);
 }
 
 static void click_config_provider(void *context) {
@@ -83,10 +112,16 @@ static void main_window_load(Window *window) {
   text_layer_set_background_color(s_output_layer, GColorRed);
   text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
+  
+  s_bitmap = gbitmap_create_with_resource(FOX_ICON[0]);
+  s_bitmap_layer = bitmap_layer_create(bounds);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bitmap_layer));
 }
 
 static void main_window_unload(Window *window) {
   text_layer_destroy(s_output_layer);
+  gbitmap_destroy(s_bitmap);
+  bitmap_layer_destroy(s_bitmap_layer);
 }
 
 static void init(void) {
